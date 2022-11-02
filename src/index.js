4afde284c5a0e2ecd3b7680ef8aab851860453ef
noguchihdk <Hideki.Noguchi,bp@nttdata.com>
2022-11-02 11:26:00 +0900

    changes for v3

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth0Provider } from "@auth0/auth0-react";
+import React from "react";
+import ReactDOM from "react-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
+import App from "./App";
+
+ReactDOM.render(
+  <Auth0Provider
+    domain="nttdata-partner.auth0.com"
+    clientId="PSBpf1hFP1t3YwzchJ9cN7qm2XJHfqaD"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
