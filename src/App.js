090c67f6a258ced5e8590d9f1abbafd75af0374e
noguchihdk <Hideki.Noguchi,bp@nttdata.com>
2022-11-14 18:45:05 +0900

    20221114

--- src/App.js
+++ src/App.js
@@ -15,7 +15,27 @@ function App() {
   if (isAuthenticated) {
     return (
       <div>
-        Hello {user.name}{' '}
+        Hello name:{user.name}{' '}
+        address:{user.address}{' '}
+        birthdate:{user.birthdate}{' '}
+        email:{user.email}{' '}
+        email_verified:{user.email_verified}{' '}
+        family_name:{user.family_name}{' '}
+        gender:{user.gender}{' '}
+        given_name:{user.given_name}{' '}
+        locale:{user.locale}{' '}
+        middle_name:{user.middle_name}{' '}
+        name:{user.name}{' '}
+        nickname:{user.nickname}{' '}
+        phone_number:{user.phone_number}{' '}
+        phone_number_verified:{user.phone_number_verified}{' '}
+        picture:{user.picture}{' '}
+        preferred_username:{user.preferred_username}{' '}
+        profile:{user.profile}{' '}
+        sub:{user.sub}{' '}
+        updated_at:{user.updated_at}{' '}
+        website:{user.website}{' '}
+        zoneinfo:{user.zoneinfo}{' '}
         <button onClick={() => logout({ returnTo: window.location.origin })}>
           Log out
         </button>
